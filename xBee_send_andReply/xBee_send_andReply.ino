2a1438f19a0fb11412b1a85f716fdbd5f82216f0
Steven Brice <brice.steven@gmail.com>
2012-11-17 00:01:52 -0600

    Initial Commit

---
+++ xBee_send_andReply/xBee_send_andReply.ino
@@ -0,0 +1,24 @@
+#define pinBtn 8
+
+unsigned long lastTime = 0;
+unsigned long currTime = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinBtn, INPUT);
+}
+
+
+void loop() {
+  delay(5000);
+   Serial.write('A');
+   while(true) {
+  if(Serial.read() == 'B') {
+    currTime = micros();
+    Serial.print((currTime-lastTime));
+    Serial.write('A');
+    Serial.println();
+    lastTime=currTime;
+  }  
+   }
+}
---
+++ xBee_send_simple/xBee_send_simple.ino
@@ -0,0 +1,13 @@
+#define pinBtn 8
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(pinBtn, INPUT);
+}
+
+
+void loop() {
+  Serial.write('A');
+  delay(1000);
+  
+}
