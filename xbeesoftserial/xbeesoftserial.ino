b870f1266897b92b66eaf9acc176a817c223f45e
Steven Brice <brice.steven@gmail.com>
2012-11-29 20:54:21 -0600

    inital Commit
    read xBee through software serial

---
+++ xbeesoftserial/xbeesoftserial.ino
@@ -0,0 +1,24 @@
+#include "SoftwareSerial.h"
+
+#define TX 3
+#define RX 2
+//#include <NewSoftSerial.h>
+
+// set up a new serial port
+//NewSoftSerial xbee =  NewSoftSerial(RX, TX);
+SoftwareSerial xbee(RX,TX);
+
+
+void setup()
+{
+ xbee.begin(9600);
+ Serial.begin(9600); 
+}
+
+void loop()
+{
+  char data = xbee.read();
+  if(data == 'B') {
+    Serial.write(data);
+  }
+}
